f805d03bd919676127e4cf9b97c9dfca39450002
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-11-30 00:34:00 -0800

    Create 1512. Number of Good Pairs.js

---
+++ 1512. Number of Good Pairs.js
@@ -0,0 +1,11 @@
+var numIdenticalPairs = function(nums) {
+    let counter = 0;
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] == nums[j] && i < j){
+                counter++;
+            }
+        }
+    }
+   return counter; 
+};
