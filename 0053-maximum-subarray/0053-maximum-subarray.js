8a2a6e9227fd32ac9b1290957326a736612d0d9a
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-17 05:41:10 -0800

    Time: 96 ms (82.09%), Space: 50.5 MB (33.56%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,9 @@
+
+var maxSubArray = function(nums) {
+    let max = nums[0]
+    for(let i = 1; i < nums.length;i++){
+         nums[i] = Math.max(nums[i],nums[i] + nums[i - 1])
+         max = Math.max(nums[i],max)         
+    }
+    return max
+};
