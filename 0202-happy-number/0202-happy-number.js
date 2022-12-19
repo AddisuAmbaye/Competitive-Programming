8159c44d952aaba5482eaf95674725dc80c31973
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-18 23:18:02 -0800

    Time: 64 ms (96.78%), Space: 42.2 MB (93.96%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,15 +3,19 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
+    
     const set = new Set()
+    
  while(n !== 1){
         sum = 0
      while(n > 0){
-       sum += Math.pow( n % 10,2)
+         
+       sum += (n % 10)*(n % 10)
        n = Math.floor(n/10)
      }
     
     if(set.has(sum)){
+        
         return false
     }
     else{
