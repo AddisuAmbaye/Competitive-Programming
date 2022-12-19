4e28f0625fbccc1af97ca3cb83bec467e11aa38d
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-19 00:15:17 -0800

    Time: 177 ms (17.51%), Space: 49.7 MB (21.54%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -6,9 +6,8 @@ var reverseString = function (s) {
   let left = 0, right = s.length - 1;
 
   while (left < right) {
-    const temp = s[left]
-    s[left] = s[right]
-    s[right] = temp
+  
+      [s[right],s[left] ]= [s[left],s[right]]
 
     left++
     right--
