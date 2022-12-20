2adbbb5f3fae331b09b57d3721b738af02f5f634
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-19 19:37:48 -0800

    Time: 56 ms (99.12%), Space: 43.8 MB (39.52%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -3,25 +3,16 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
 var intersection = function(nums1, nums2) {
-   const arr = []
-const set1 = new Set()
-const map = new Map()
-    for(i = 0; i < nums1.length; i++){
-       
-        set1.add(nums1[i])
-    }
-    for(j = 0; j < nums2.length; j++){
-       
-        map.set(j,nums2[j])
-    }
-    n = map.size - 1
-    while(n >= 0){
-       if(set1.has(map.get(n)) && arr.includes(map.get(n)) == false){
-          arr.push(map.get(n))
-          
-    }
-       n--
-  }
-    return arr
+    nums1 = new Set(nums1)
+    nums2 = new Set(nums2)
+    
+    if (nums1.size > nums2.size) {
+        temp  = nums2
+        nums2 = nums1
+        nums1 = temp
+}
+    
+    return [...nums1].filter(num => nums2.has(num))
 };
