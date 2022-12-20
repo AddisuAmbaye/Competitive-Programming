9ab7c284c9e42280a11312e7566f721d6a0a95a5
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-19 23:20:27 -0800

    Time: 113 ms (29.28%), Space: 42.4 MB (82.29%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+
+var intersect = function(nums1, nums2) {
+    let base, sub
+    if(nums1.length > nums2.length ) {
+        base = nums1
+        sub = nums2
+    }else {
+        base = nums2
+        sub = nums1
+    }
+    let res = [] 
+    for(let i = 0 ; i < sub.length ; i++){
+        if(base.includes(sub[i])){
+            res.push(sub[i])
+            base[base.indexOf(sub[i])] = null
+        }
+    }
+    return res
+};
