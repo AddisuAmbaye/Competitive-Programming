8faa5c9157de41a74f92e4108a355b0ea5697874
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-22 04:58:19 -0800

    Time: 65 ms (88.55%), Space: 42.3 MB (32.33%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var distinctAverages = function(nums) {
+    left = 0
+    right = nums.length - 1
+    const set = new Set()
+    nums.sort((a, b) => a - b)
+    while(left < right){
+       ave = (nums[left] + nums[right])/2
+    
+        set.add(ave)
+        left++
+        right--
+    }
+    return set.size
+};
