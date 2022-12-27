bbd016e495565b1bcfd26f0d267337ac0e4b0d4d
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-26 23:40:13 -0800

    Time: 95 ms (91.95%), Space: 45.2 MB (32.44%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -7,8 +7,8 @@ var findContentChildren = function(g, s) {
     count = 0
     j = 0
     i = 0
-    g.sort((a,b) => a - b)
-    s.sort((a,b) => a - b)
+     g.sort((a,b) => a - b)
+     s.sort((a,b) => a - b)
     while(i < s.length){
          if(s[i] >= g[j]){
             count++
