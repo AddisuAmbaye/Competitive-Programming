a0adc192d91f26d28b8d75a054264494d343d962
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2022-12-29 23:58:31 -0800

    Time: 63 ms (94.76%), Space: 41.9 MB (83.77%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.js
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -7,9 +7,9 @@ var isLongPressedName = function(name, typed) {
     let p1 = 0;
     let p2 = 0;
     while (p1 < name.length || p2 < typed.length) {
-        if (name[p1] == typed[p2]) p1 ++, p2++;
-        else if (name[p1 - 1] == typed[p2]) p2 ++;
+        if (name[p1] === typed[p2]) p1 ++, p2++;
+        else if (name[p1 - 1] === typed[p2]) p2 ++;
         else return false;
     }
-    return p1 == name.length;
+    return p1 === name.length;
 };
