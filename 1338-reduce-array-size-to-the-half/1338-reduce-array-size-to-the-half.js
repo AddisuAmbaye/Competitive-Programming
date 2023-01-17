9d3fb140e2659562cf094108ee8ed0e70dd4ecb2
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2023-01-17 13:49:38 -0800

    Time: 384 ms (20.28%), Space: 65.9 MB (65.04%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function(arr) {
+        n = arr.length
+        map = {}
+ for (let i = 0; i < arr.length; i++){
+     
+   map[arr[i]] = (map[arr[i]] || 0) + 1
+     
+   }
+
+        sum = 0
+        count = 1
+    arr.sort((a,b) => map[b] - map[a] || b - a)
+    arr_set = [...new Set(arr)]
+    for(i = 0; i < arr_set.length; i++){
+    sum += map[arr_set[i]]
+    if(sum >= n/2){
+        break
+      }
+    count++
+    }
+  return count
+};
