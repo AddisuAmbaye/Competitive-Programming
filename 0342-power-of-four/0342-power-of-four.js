47e00eaa27f390f31ba6adfabf6ff83c782f886e
Addis Ambaye <101856038+AddisuAmbaye@users.noreply.github.com>
2023-01-25 11:13:10 -0800

    Time: 89 ms (34.24%), Space: 42.4 MB (87.48%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    while(n >= 4){
+       if(n % 4 != 0) return false
+       n = n / 4
+    } 
+    return n == 1
+};
